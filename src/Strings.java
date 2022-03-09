038a8530541bd9805555c310117b697b0845164f
SaranyaKumaresans <101115917+SaranyaKumaresans@users.noreply.github.com>
2022-03-09 18:43:00 +0530

    My first commit

---
+++ src/Strings.java
@@ -0,0 +1,13 @@
+
+public class Strings {
+	public void Reverse() {
+		System.out.println("the word is :saranya");
+	}
+	
+		
+		public static void main(Strings[] args) {
+		Strings sb=new Strings();  
+		sb.Reverse();  
+		System.out.println(sb); 
+		}  
+		}  
